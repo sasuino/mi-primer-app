4942ed4e1f63687f76aecbc69074a53f718ad8c7
sasuino <102831661+sasuino@users.noreply.github.com>
2022-04-01 15:00:36 -0700

    .

--- Myappnoentendi.py
+++ Myappnoentendi.py
@@ -13,3 +13,7 @@ import pandas as pd
 #st.latex("\int_1^6")
 #st.markdown("#titulo")
 #st.markdown("este es una viñeta")
+num1 = st.slider('Elige el numero 1', 0, 100, 25)
+num2 = st.slider('Elige el numero 2', 0, 100, 25)
+suma=num1+num2
+st.write("La suma de",num1,"y",num2, 'es:',suma)
