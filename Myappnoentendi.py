f4841219321d078cab43e15151fda764d38ce43e
sasuino <102831661+sasuino@users.noreply.github.com>
2022-04-01 14:32:42 -0700

    .

--- Myappnoentendi.py
+++ Myappnoentendi.py
@@ -2,6 +2,7 @@ import streamlit as st
 st.title("mi primer app")
 #st.button("ok")
 import pandas as pd
-df=pd.read_csv("https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv")
-st.write(df)
-st.map(df)
+#Las primeras 3 lineas son para el primer ejercicio
+#df=pd.read_csv("https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv")
+#st.write(df)
+#st.map(df)
