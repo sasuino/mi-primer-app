32a760a530b2a37310d4dc7ef8e0378fc5d81799
sasuino <102831661+sasuino@users.noreply.github.com>
2022-04-01 15:11:18 -0700

    .

--- Myappnoentendi.py
+++ Myappnoentendi.py
@@ -22,3 +22,7 @@ nm1=st.number_input('Dame n1')
 nm2=st.number_input('Dame n2')
 mult=nm1*nm2
 st.write('La multiplicacion de', nm1,"por",nm2,"es :",mult)
+with st.sidebar:
+    num1 = st.slider('Elige el numero 1', 0, 100, 25)
+    num2 = st.slider('Elige el numero 2', 0, 100, 25)
+    
